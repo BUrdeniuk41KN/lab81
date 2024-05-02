c9644b883f4ceb64e101dd74686fab8a4aa67a59
Hatsumi <burdenjuk.bohdan.clg@chnu.edu.ua>
2024-05-02 18:33:36 +0200

    Commit fixed port json-server

--- functions/server.js
+++ functions/server.js
@@ -1,27 +1,24 @@
 const { exec } = require('child_process');
 
 exports.handler = async (event, context) => {
-  let log = "";
+  let log = null;
   // Запустить json-server через командную строку
   exec('json-server --watch TestDB.json --port 3005', (error, stdout, stderr) => {
     if (error) {
-      log += "Ошибка запуска json-server: " + error.message;
       console.error(`Ошибка запуска json-server: ${error.message}`);
       return;
     }
     if (stderr) {
-      log += "\nОшибка вывода json-server: " + stderr;
       console.error(`Ошибка вывода json-server: ${stderr}`);
       return;
     }
-    log += "\njson-server запущен: " + stdout;
     console.log(`json-server запущен: ${stdout}`);
-
+    log = stdout;
   });
 
   // Вернуть ответ, например, для CORS
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Сервер JSON успешно запущен\n" + log})
+    body: JSON.stringify({ message: "Сервер JSON успешно запущен: " + log})
   };
 };
