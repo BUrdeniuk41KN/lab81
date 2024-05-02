cea3f61d070169ac2031d008ff90f8fef1d53f6d
Hatsumi <burdenjuk.bohdan.clg@chnu.edu.ua>
2024-05-02 23:08:16 +0200

    Commit test fix for deploy server

--- functions/server.js
+++ functions/server.js
@@ -10,25 +10,25 @@ exports.handler = async (event, context) => {
 
   // Команда для запуска json-server
   const startCommand = 'json-server --watch db.json --port 3005';
-  
+
   // Выполнение установочной команды
   exec(installCommand, (error, stdout, stderr) => {
     if (error) {
       console.error(`Ошибка установки json-server: ${error}`);
       return;
     }
-  
+
     console.log('json-server успешно установлен');
-  
-    // Выполнение команды для запуска json-server после успешной установки
-    exec(startCommand, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Ошибка запуска json-server: ${error}`);
-        return;
-      }
-      console.log(`json-server успешно запущен: ${stdout}`);
-    });
   });
+  // Выполнение команды для запуска json-server после успешной установки
+  exec(startCommand, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Ошибка запуска json-server: ${error}`);
+      return;
+    }
+    console.log(`json-server успешно запущен: ${stdout}`);
+  });
+
 };
 
 async function fetchData() {
